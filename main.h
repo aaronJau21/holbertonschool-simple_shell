2133000db2dd93013badac43bd3f55a4ea2c4619
Aaron <aaronjau21@gmail.com>
2022-08-07 01:36:45 -0500

    adding the final part of our main.h file

--- main.h
+++ main.h
@@ -33,3 +33,35 @@ void create_child(char **param_array, char *line, int count, char **av);
 char **token_interface(char *line, const char *delim, int token_count);
 char **tokenize(int token_count, char *line, const char *delim);
 int count_token(char *line, const char *delim);
+/*helpers*/
+int str_len(char *str);
+void double_free(char **to_be_freed);
+void single_free(int n, ...);
+void error_printing(char *av, int count, char *command);
+void exec_error(char *av, int count, char *tmp_command);
+
+/*number.c*/
+int print_number(int n);
+
+
+/*hbtlib*/
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *src);
+void print_str(char *str, int new_line);
+int _write_char(char c);
+
+/*path*/
+char *path_finder(char *command);
+int find_path(char *str);
+char **tokenize_path(int index, char *str);
+char *search_directories(char **path_tokens, char *command);
+char *build_path(char *directory, char *command);
+
+/*builtins*/
+void cd_b(char *line);
+void env_b(__attribute__((unused))char *line);
+void exit_b(char *line);
+void (*check_built_ins(char *str))(char *str);
+int built_in(char **command, char *line);
+
+#endif
